f4f31e24048ac44e30cfec69302b5bfeec8cd89e
anthonyjrade3 <remotemouseemail@gmail.com>
2025-03-13 19:18:44 +0200

    Updated .gitignore

--- commands/utilities/avatar.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'avatar',
-    description: 'Get a user’s avatar',
-    execute(message) {
-        const user = message.mentions.users.first() || message.author;
-        message.reply(user.displayAvatarURL({ dynamic: true, size: 512 }));
-    }
-};
